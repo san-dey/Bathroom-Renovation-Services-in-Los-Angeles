50dbd92e1aa8112725d787bf11515a338904ab38
san-dey <najvimahil@gmail.com>
2025-06-29 19:58:07 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'Bathroom Renovation Services in Los Angeles'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
